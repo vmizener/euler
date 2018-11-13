98f69d7d020d8e91a61a8bee3172b029ff2ede22
Raymond von Mizener <vmizener@gmail.com>
2018-11-13 13:28:15 -0800

    Typo

--- src/problems/p0099/main.py
+++ src/problems/p0099/main.py
@@ -5,7 +5,7 @@ def main():
         exponentials = [[int(item) for item in line.strip().split(',')] for line in fh]
 
     # Use logarithms
-    # I am embarassed to say it took me way to long to thing of that
+    # I am embarassed to say it took me way to long to think of that
     best_idx, best_val = None, None
     for idx, (next_base, next_exp) in enumerate(exponentials):
         next_val = next_exp * log(next_base)
